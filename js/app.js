f7ebe514bf95942c692cb5a40b298a864dca633f
Charlie <chazzahorn@gmail.com>
2023-03-28 14:12:40 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('domcontentloaded');
+    
+    const destinationCreateFormElement = document.querySelector('#destination-create-form');
+    destinationCreateFormElement.addEventListener('submit', handleCreateFormSubmit);
+
+    // const 
+})
+
+const handleCreateFormSubmit = function (event) {
+    event.preventDefault();
+    const formResponseList = document.querySelector('#form-response');
+    const formInputs = ['destination_name', 'destination_country', 'destination_city', 'destination_type', 'destination_description', 'destination_image']
+
+    for ( const item of formInputs ) {
+        const listElement = document.createElement('li');
+        listElement.textContent = `${item.slice(12)}: ${this[item].value}`;
+        formResponseList.appendChild(listElement)
+    }
+    
+}
